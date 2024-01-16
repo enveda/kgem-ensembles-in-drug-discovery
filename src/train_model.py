670285c74b7fee97706311a3453930000b1d24f1
ddomingof <danieldomingofernandez@hotmail.com>
2024-01-16 15:03:02 +0100

    Update parameters and improve docs

--- src/train_model.py
+++ src/train_model.py
@@ -3,11 +3,11 @@
 """Script to run the train model for link prediction.
 
 Ex. >> run_pykeen(
-        train_file_path='../data/model_data/custom_train.tsv',
-        test_file_path='../data/model_data/custom_test.tsv',
-        val_file_path='../data/model_data/custom_val.tsv',
+        train_file_path='../data/kg/train.tsv',
+        test_file_path='../data/kg/test.tsv',
+        val_file_path='../data/kg/val.tsv',
         output_directory='../data/models',
-        configuration='mure'
+        configuration='MuRe'
     )
 """
 
@@ -22,7 +22,7 @@ from pykeen.triples import TriplesFactory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 PROJECT_DIR = os.path.join(os.path.abspath(os.path.join(dir_path, os.pardir)))
 
-logger = logging.getLogger('__name__')
+logger = logging.getLogger("__name__")
 
 
 def run_pykeen(
@@ -30,7 +30,7 @@ def run_pykeen(
     test_file_path: str,
     val_file_path: str,
     output_directory: str,
-    configuration: str = 'TransE',
+    configuration: str = "TransE",
 ):
     """Embed the given graph using the model and used for link prediction.
     :param train_file_path: directory to TSV for training triples
@@ -40,15 +40,17 @@ def run_pykeen(
     :param configuration: the model name that you want to use for embedding
     :return hpo_pipeline that holds results for model
     """
-    with open(os.path.join(PROJECT_DIR, 'kgem-params', f'{configuration}.json'), 'r') as config_file:
+    with open(
+        os.path.join(PROJECT_DIR, "data", "kgem-params", f"{configuration}.json"), "r"
+    ) as config_file:
         model_config = json.load(config_file)
 
-    logger.info(f'running {configuration} model')
+    logger.info(f"running {configuration} model")
 
     # Load the data sets
-    train_df = pd.read_csv(train_file_path, sep='\t').to_numpy()
-    test_df = pd.read_csv(test_file_path, sep='\t').to_numpy()
-    val_df = pd.read_csv(val_file_path, sep='\t').to_numpy()
+    train_df = pd.read_csv(train_file_path, sep="\t").to_numpy()
+    test_df = pd.read_csv(test_file_path, sep="\t").to_numpy()
+    val_df = pd.read_csv(val_file_path, sep="\t").to_numpy()
 
     train = TriplesFactory.from_labeled_triples(train_df)
     test = TriplesFactory.from_labeled_triples(test_df)
@@ -65,7 +67,7 @@ def run_pykeen(
         loss=model_config["pipeline"]["loss"],
         loss_kwargs=model_config["pipeline"].get("loss_kwargs"),
         loss_kwargs_ranges=model_config["pipeline"].get("loss_kwargs_ranges"),
-        regularizer=model_config["pipeline"].get("regularizer"),
+        # regularizer=model_config["pipeline"].get("regularizer"),
         optimizer=model_config["pipeline"]["optimizer"],
         optimizer_kwargs=model_config["pipeline"].get("optimizer_kwargs"),
         optimizer_kwargs_ranges=model_config["pipeline"].get("optimizer_kwargs_ranges"),
@@ -74,13 +76,15 @@ def run_pykeen(
         training_kwargs_ranges=model_config["pipeline"].get("training_kwargs_ranges"),
         negative_sampler=model_config["pipeline"].get("negative_sampler"),
         negative_sampler_kwargs=model_config["pipeline"].get("negative_sampler_kwargs"),
-        negative_sampler_kwargs_ranges=model_config["pipeline"].get("negative_sampler_kwargs_ranges"),
+        negative_sampler_kwargs_ranges=model_config["pipeline"].get(
+            "negative_sampler_kwargs_ranges"
+        ),
         stopper=model_config["pipeline"].get("stopper"),
         stopper_kwargs=model_config["pipeline"].get("stopper_kwargs"),
         evaluator=model_config["pipeline"].get("evaluator"),
         evaluator_kwargs=model_config["pipeline"].get("evaluator_kwargs"),
         evaluation_kwargs=model_config["pipeline"].get("evaluation_kwargs"),
-        n_trials=model_config["optuna"]['n_trials'],
+        n_trials=model_config["optuna"]["n_trials"],
         timeout=model_config["optuna"]["timeout"],
         metric=model_config["optuna"]["metric"],
         direction=model_config["optuna"]["direction"],
@@ -92,3 +96,15 @@ def run_pykeen(
     os.makedirs(output_directory, exist_ok=True)
     results.save_to_directory(output_directory)
     return results
+
+
+##### Example on how to run the model #####
+##### Modify here the model you want #####
+# if "__main__" == __name__:
+#     run_pykeen(
+#         train_file_path="./data/kg/biokg/train.tsv",
+#         test_file_path="./data/kg/biokg/test.tsv",
+#         val_file_path="./data/kg/biokg/val.tsv",
+#         output_directory="./data/",
+#         configuration="ConvE",
+#     )
